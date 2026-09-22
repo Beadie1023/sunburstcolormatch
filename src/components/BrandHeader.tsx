cedd885ba762ecf868b68f71b4d919c6378ca922
Beadie1023 <44064754+Beadie1023@users.noreply.github.com>
2026-09-22 07:25:34 -0400

    Switch logo asset to vector version of supplied design

--- src/components/BrandHeader.tsx
+++ src/components/BrandHeader.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import logo from "@/assets/sunburst-logo.png";
+import logo from "@/assets/sunburst-logo.svg";
 
 export function BrandHeader({ subtitle }: { subtitle?: string }) {
   return (
